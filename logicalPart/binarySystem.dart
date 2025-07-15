58f0ed86f3c22288a33281d299f707d8e52ced57
Ukasha-git-hub <ukashakhursheed@gmail.com>
2025-07-15 18:27:04 +0500

    testing during practice

--- logicalPart/binarySystem.dart
+++ logicalPart/binarySystem.dart
@@ -14,15 +14,16 @@ int BtoD(int Bn) {
   int reminder = 0;
   int power = 1;
   while (Bn > 0) {
-    int lastdig = Bn % 10;
-    reminder += lastdig * power;
-    Bn ~/= 10;
-    power *= 2;
+    int lastdig = Bn % 10;  // separating last digit
+    reminder += lastdig * power;  //storing  last digit into variable 
+    Bn ~/= 10;   // updating value
+    power *= 2; // updating power 
   }
   return reminder;
 }
 
 void main() {
   print(BtoD(1010));
+   print(BtoD(10011));
   print(decimaltobinaryConversion(10));
 }
--- logicalPart/testing.dart
+++ logicalPart/testing.dart
@@ -1,51 +1,51 @@
-// // import 'dart:math';
+import 'dart:math';
 
-// // void main() {
-// //   nPrime(11);
-// // }
+void main() {
+  nPrime(11);
+}
 
-// // void nPrime(int n) {
-// //   int sum = 0;
-// //   for (int i = 2; i <= n; i++) {
-// //     if (isPrime(i)) {
-// //       print(i);
+void nPrime(int n) {
+  int sum = 0;
+  for (int i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      print(i);
 
-// //     }
+    }
 
-// //   }
-// //       print(sum);
+  }
+      print(sum);
 
-// // }
+}
 
-// // bool isPrime(int nums) {
-// //   if (nums <= 1) {
-// //     return false;
-// //   }
-// //   for (int i = 2; i <= sqrt(nums).toInt(); i++) {
-// //     if (nums % i == 0) {
-// //       return false;
-// //     }
-// //   }
-// //   return true;
-// // }
-// void main() {
-//   fibonacci(13);
-// }
+bool isPrime(int nums) {
+  if (nums <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= sqrt(nums).toInt(); i++) {
+    if (nums % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+void main1() {
+  fibonacci(13);
+}
 
-// void fibonacci(int n) {
-//   int a = 0;
-//   int b = 1;
-//   for (int i = 0; i <= n; i++) {
-//     print(a);
-//     int next = a + b;
+void fibonacci(int n) {
+  int a = 0;
+  int b = 1;
+  for (int i = 0; i <= n; i++) {
+    print(a);
+    int next = a + b;
 
-//        a = b;
-//     b = next;
+       a = b;
+    b = next;
 
-//     // print(b);
+    // print(b);
 
-//   }
-// }
+  }
+}
 dynamic decimaltobinaryConversion(int Dn) {
   dynamic ans = 0;
   dynamic power = 1;
@@ -70,7 +70,7 @@ int BtoD(int Bn) {
   return reminder;
 }
 
-void main() {
+void main2() {
   print(BtoD(1010));
   print(decimaltobinaryConversion(10));
 }
