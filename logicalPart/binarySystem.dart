6aed9bb984f22378509540367bd75115d43ec199
Ukasha-git-hub <ukashakhursheed@gmail.com>
2025-07-17 17:31:04 +0500

    bitwise and homeWork

--- logicalPart/binarySystem.dart
+++ logicalPart/binarySystem.dart
@@ -21,8 +21,24 @@ int BtoD(int Bn) {
   }
   return reminder;
 }
+void check(int n) {
+  int ans = 0;
+  // int power = 1;
+  while (n > 0) {
+    int last = n % 10;
+    ans = ans * 10 + last;
+    // ans = last * power + ans;
+    n ~/= 10;
+    // power *= 10;
+    // stdout.write(ans);
+  }
+  print(ans);
+}
+
+
 
 void main() {
+    check(123456789);
   print(BtoD(1010));
    print(BtoD(10011));
   print(decimaltobinaryConversion(10));
